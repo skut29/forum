038e4efdbe6502e5fc195486e8d6967bf0bd2173
Sarah Abdulaleem <amyourgirl16@gmail.com>
2021-09-09 11:15:12 -0400

    Adding header

---
+++ src/components/Nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div className="Nav">
+            <h1>Forums</h1>
+            <button>login</button>
+        </div>
+    )
+}
+
+export default Nav
